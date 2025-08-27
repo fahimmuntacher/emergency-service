9c2348959dd1115fe1cccee04f3bca2e593301f6
fahimmuntacher <muntacherfahim234@gmail.com>
2025-08-27 15:51:40 +0600

    card updated

--- index.js
+++ index.js
@@ -11,14 +11,21 @@ function getElementClass(id){
 }
 
 /* ***************** Heart Button  ******************************************/
-const hearts = document.getElementsByClassName('hearts'); 
-for(let heart of hearts){
-    heart.addEventListener('click', function(){
+getElementId('card-box').addEventListener("click", function(h){
+    if(h.target.className.includes("hearts")){
         const heartTotal = getElementId("heart").innerText;
-        getElementId("heart").innerText = Number(heartTotal) + 1; 
+        getElementId("heart").innerText = Number(heartTotal) + 1;
+    }
+})
+
+// const hearts = document.getElementsByClassName('hearts'); 
+// for(let heart of hearts){
+//     heart.addEventListener('click', function(){
+//         const heartTotal = getElementId("heart").innerText;
+//         getElementId("heart").innerText = Number(heartTotal) + 1; 
         
-    })
-}
+//     })
+// }
 
 
 /* ***************** Call Button ******************************************/
@@ -35,15 +42,15 @@ getElementId('card-box').addEventListener("click", function(e){
             alert("You have Insufficient coins!")
             return
         }
-        else{
-            const chargeCoin = Number(coins) - 20; 
-            alert("Calling to " + cardTitle + " : " + tollNum)
-            document.getElementById("coins").innerText = chargeCoin;
-        }
-         const historyContainer = document.getElementById("history-container");
-            const newHitoryCart = document.createElement("div");
-            newHitoryCart.innerHTML = `
-                 <div class="bg-[#FAFAFA] shadow-xl rounded-2xl flex items-center justify-around w-10/12 mx-auto mb-8">
+        
+        const chargeCoin = Number(coins) - 20; 
+        alert("Calling to " + cardTitle + " : " + tollNum)
+        document.getElementById("coins").innerText = chargeCoin;
+
+        const historyContainer = document.getElementById("history-container");
+        const newHitoryCart = document.createElement("div");
+        newHitoryCart.innerHTML = `
+                 <div class="bg-[#FAFAFA] shadow-xl rounded-2xl flex items-center justify-between p-5 w-10/12 mx-auto mb-8">
                         <div class="p-4">
                         <h1 class="text-xl font-bold">${cardTitle}</h1>
                             <h1 class="text-2xl font-bold text-gray-500">${tollNum}</h1>
